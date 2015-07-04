749511737c2c290d9bce5d015ece7e1c58138945
Akaash Sinha <grendelfearsbeowufl@gmail.com>
2015-07-04 11:06:22 -0400

    Updated FIP
    Made it more accurate. Did not account for HBP and added the 2015
    constant.

--- fip.py
+++ fip.py
@@ -1,8 +1,10 @@
 hr = input("How many homeruns did give up ")
 bb = input("How many walks did he give ")
+h = input("How many batters did he hit ")
 k = input("How many strikeouts did he give ")
 ip = input("How many innings did he pitch ")
-fi = (13 * hr) + (3 * bb) - (2 * k) 
+batters = h + bb
+fi = (13 * hr) + (3 * batters) - (2 * k)
 fi2 = fi / float(ip)
-fip = fi2 + 3.10	
+fip = fi2 + 3.084
 print fip
